2d7e72629f47457ad8b7f7f68fc72364c56591fe
GamakCZ <gamak.mcpe@gmail.com>
2017-07-20 23:31:26 +0200

    Probably works delete command

--- MultiWorld/src/MultiWorld/Command/MultiWorldCommand.php
+++ MultiWorld/src/MultiWorld/Command/MultiWorldCommand.php
@@ -9,7 +9,6 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 use pocketmine\Server;
-use SwagCore\SwagCore;
 
 class MultiWorldCommand {
 
@@ -23,13 +22,14 @@ class MultiWorldCommand {
     public function __construct(MultiWorld $plugin) {
         $this->plugin = $plugin;
     }
-        /**
-         * @param CommandSender $sender
-         * @param Command $cmd
-         * @param string $label
-         * @param array $args
-         * @return bool
-         */
+
+    /**
+     * @param CommandSender $sender
+     * @param Command $cmd
+     * @param $label
+     * @param array $args
+     * @return bool
+     */
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args) {
         if (!($sender instanceof Player)) {
             return false;
@@ -57,11 +57,11 @@ class MultiWorldCommand {
                             return false;
                         }
                         if (empty($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("create-usage"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("create-usage"));
                             return false;
                         }
                         $this->plugin->bgenerator->generateLevel($args[1], $args[2], $args[3]);
-                        $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("create.generating")));
+                        $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("create.generating")));
                         return false;
                     case "teleport":
                     case "tp":
@@ -71,45 +71,45 @@ class MultiWorldCommand {
                             return false;
                         }
                         if (empty($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("teleport-usage"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("teleport-usage"));
                             return false;
                         }
                         if (!Server::getInstance()->isLevelGenerated($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("teleport-levelnotexists"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("teleport-levelnotexists"));
                             return false;
                         }
                         if (!Server::getInstance()->isLevelLoaded($args[1])) {
                             Server::getInstance()->loadLevel($args[1]);
-                            $this->plugin->getLogger()->debug(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-load")));
+                            $this->plugin->getLogger()->debug(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-load")));
                         }
                         if (isset($args[2])) {
                             $player = Server::getInstance()->getPlayer($args[2]);
                             if ($player->isOnline()) {
                                 $player->teleport(Server::getInstance()->getLevelByName($args[1])->getSafeSpawn(), 0, 0);
-                                $player->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-done-1")));
-                                $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], str_replace("%2", $player->getName(), LanguageManager::translateMessage("teleport-done-2"))));
+                                $player->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-done-1")));
+                                $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], str_replace("%2", $player->getName(), LanguageManager::translateMessage("teleport-done-2"))));
                                 return false;
                             } else {
-                                $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("teleport-playernotexists"));
+                                $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("teleport-playernotexists"));
                                 return false;
                             }
                         } else {
                             $sender->teleport(Server::getInstance()->getLevelByName($args[1])->getSafeSpawn(), 0, 0);
-                            $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-done-1")));
+                            $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("teleport-done-1")));
                         }
-                        break;
+                        return false;
                     case "import":
                         if (!$sender->hasPermission("mw.cmd.import")) {
                             $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
                             return false;
                         }
                         if (empty($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("import-usage"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("import-usage"));
                             return false;
                         }
                         $zipPath = ConfigManager::getDataPath() . "levels/{$args[1]}.zip";
                         if (!file_exists($zipPath)) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("import-zipnotexists"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("import-zipnotexists"));
                             return false;
                         }
                         $zip = new \ZipArchive;
@@ -118,7 +118,7 @@ class MultiWorldCommand {
                         $zip->close();
                         unset($zip);
                         Server::getInstance()->loadLevel($args[1]);
-                        $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("import-done"));
+                        $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("import-done"));
                         return false;
                     case "list":
                     case "ls":
@@ -132,7 +132,7 @@ class MultiWorldCommand {
                         unset($list[0]);
                         unset($list[1]);
                         $list = implode(", ", $list);
-                        $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $list, LanguageManager::translateMessage("list-done")));
+                        $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $list, LanguageManager::translateMessage("list-done")));
                         return false;
                     case "load":
                         if (!$sender->hasPermission("mw.cmd.load")) {
@@ -140,15 +140,15 @@ class MultiWorldCommand {
                             return false;
                         }
                         if (empty($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("load-usage"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("load-usage"));
                             return false;
                         }
                         if (!Server::getInstance()->isLevelGenerated($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("load-levelnotexists")));
+                            $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("load-levelnotexists")));
                             return false;
                         }
                         Server::getInstance()->loadLevel($args[1]);
-                        $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("load-done")));
+                        $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("load-done")));
                         break;
                     case "unload":
                         if (!$sender->hasPermission("mw.cmd.unload")) {
@@ -156,27 +156,87 @@ class MultiWorldCommand {
                             return false;
                         }
                         if (empty($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("unload-usage"));
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("unload-usage"));
                             return false;
                         }
                         if (!Server::getInstance()->isLevelGenerated($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-levelnotexists")));
+                            $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-levelnotexists")));
                             return false;
                         }
                         if (!Server::getInstance()->isLevelLoaded($args[1])) {
-                            $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-unloaded")));
+                            $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-unloaded")));
                             return false;
                         }
                         Server::getInstance()->unloadLevel(Server::getInstance()->getLevelByName($args[1]));
-                        $sender->sendMessage(SwagCore::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-done")));
+                        $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("unload-done")));
                         return false;
+                    case "delete":
+                    case "remove":
+                        if (!$sender->hasPermission("mw.cmd.delete")) {
+                            $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
+                            return false;
+                        }
+                        if (empty($args[1])) {
+                            $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("delete-usage"));
+                            return false;
+                        }
+                        if (!Server::getInstance()->isLevelGenerated($args[1])) {
+                            $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("delete-levelnotexists")));
+                            return false;
+                        }
+
+                        $level = $this->plugin->getServer()->getLevelByName($args[1]);
+                        $folderName = $level->getFolderName();
+
+                        if (Server::getInstance()->isLevelLoaded($args[1])) {
+                            Server::getInstance()->unloadLevel($this->plugin->getServer()->getLevelByName($args[1]));
+                        }
+
+                        $worldPath = ConfigManager::getDataPath() . "worlds/";
+                        $levelPath = $worldPath . $folderName;
+
+                        if (!is_dir($levelPath)) {
+                            $sender->sendMessage("§cLevel path not found.");
+                            return false;
+                        }
+
+                        foreach (scandir($levelPath) as $directory)
+                            if (!is_dir($worldPath . $directory)) {
+                                // scanning for level.dat, ...
+                                if (is_file($worldPath . $directory)) {
+                                    unlink($worldPath . $directory);
+                                } else {
+                                    $sender->sendMessage("§cLevel can not be deleted bug #1");
+                                    return false;
+                                }
+                            }
+                        if (is_dir($worldPath . $directory)) {
+                            // scanning for region
+                            foreach (scandir($worldPath . $directory) as $file) {
+                                rmdir($file);
+                            }
+                        }
+
+
                 }
-            } else {
-                if (!$sender->hasPermission("mw.cmd.help")) {
-                    $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
-                } else {
-                    $sender->sendMessage(SwagCore::getPrefix() . LanguageManager::translateMessage("default-usage"));
+
+                if (!(count(scandir($levelPath)) <= 0)) {
+                    $sender->sendMessage("§cLevel can not be deleted bug #2");
+                    return false;
                 }
+
+                rmdir($levelPath);
+
+                $sender->sendMessage(MultiWorld::getPrefix() . str_replace("%1", $args[1], LanguageManager::translateMessage("delete-done")));
+                return false;
+            }
+        }
+        else {
+            if (!$sender->hasPermission("mw.cmd.help")) {
+                $sender->sendMessage(LanguageManager::translateMessage("not-perms"));
+            }
+            else {
+                $sender->sendMessage(MultiWorld::getPrefix() . LanguageManager::translateMessage("default-usage"));
             }
         }
 
